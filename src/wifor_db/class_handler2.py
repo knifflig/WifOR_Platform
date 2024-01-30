84a077b251fdfff782d43728aee34cbbe3424305
Henry <knifflig.utopisch-0u@icloud.com>
2024-01-30 16:33:28 +0100

    Refactor data handling with TABLE_CONNECTOR class
    Replaced independent JSON loading and class creation functions with a
    consolidated TABLE_CONNECTOR class to streamline database interactions.
    The new class introduces context management to handle session creation
    and closure, error handling, and efficient data processing with event
    listener hooks for session flushing. Additionally, data duplication
    checks and existing entry updating logic have been implemented to
    maintain data integrity upon new insertion attempts. This change aims to
    enhance code maintainability and improve the robustness of data
    operations within the database.

--- src/wifor_db/class_handler2.py
+++ src/wifor_db/class_handler2.py
@@ -1,193 +1,209 @@
 # Standard library imports
 import os
 import json
+from datetime import datetime, timedelta
+import logging
 import traceback
-from datetime import datetime
+from collections import defaultdict
 
 # Third-party imports
 import sqlalchemy
-from sqlalchemy import Column, Integer, Date
+from sqlalchemy import Column, Integer, Date, create_engine, inspect, event
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # Local application imports
 from wifor_db import _env_cache, open_log, close_log
 
-class JsonLoaderError(Exception):
-    """ Custom exception class for JSON Loader errors. """
-    pass
+class TABLE_CONNECTOR:
+    def __init__(self):
+        self.log = open_log("CONNECTOR_LOG")
+        self.Base = declarative_base()
+        self.engine = None
+        self.session = None
 
-def load_class_json(class_name: str) -> dict:
-    """
-    Load JSON data for a given class.
+    def __enter__(self):
+        self.log.info("OPEN CONNECTOR LOG")
+        self.engine = self.create_engine_from_env()
+        self.session = self.create_session(self.engine)
+        self.register_before_flush_event(self.session)
+        self.log.info("session created")
+        
+        return self
 
-    Args:
-        class_name (str): The name of the class for which the JSON is to be loaded.
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self.session:
+            self.session.close()
+            self.log.info("session closed")
+        if self.log:
+            self.log.info("CLOSE CONNECTOR LOG")
+            close_log(self.log)
 
-    Returns:
-        dict: The JSON content of the class.
-
-    Raises:
-        JsonLoaderError: If the JSON file cannot be found or read.
-    """
-    log = open_log(class_name)
-    log.info("Starting to load JSON for class: %s", class_name)
-
-    json_path = os.path.join(_env_cache['CLASS_DIR'], f"{class_name}.json")
-    log.info("""Attempting to load JSON file from path:
-                %s""", json_path)
-
-    if not os.path.exists(json_path):
-        log.error("The file %s does not exist.", json_path)
-        close_log(log)
-        raise JsonLoaderError(f"The file {json_path} does not exist.")
+    @staticmethod
+    def create_engine_from_env():
+        current_db = _env_cache['CURRENT_DB']
+        if current_db == 'sqlite':
+            db_url = _env_cache['SQLITE_DB_PATH']
+        elif current_db == 'mysql':
+            db_url = f"mysql+pymysql://{_env_cache['MYSQL_DB_USER']}:{_env_cache['MYSQL_DB_PASSWORD']}@{_env_cache['MYSQL_DB_HOST']}/{_env_cache['MYSQL_DB_NAME']}"
+        elif current_db == 'postgresql':
+            db_url = f"postgresql://{_env_cache['POSTGRES_DB_USER']}:{_env_cache['POSTGRES_DB_PASSWORD']}@{_env_cache['POSTGRES_DB_HOST']}:{_env_cache['POSTGRES_DB_PORT']}/{_env_cache['POSTGRES_DB_NAME']}"
+        else:
+            raise ValueError(f"Unsupported database type: {current_db}")
+        return create_engine(db_url)
     
-    try:
+    @staticmethod
+    def create_session(engine):
+        if not engine:
+            raise ValueError("Engine not initialized")
+        Session = sessionmaker(bind=engine)
+        return Session()
+
+##############################################################################################################
+    @staticmethod
+    def load_class_json(self, class_name):
+        json_path = os.path.join(_env_cache['CLASS_DIR'], f"{class_name}.json")
         with open(json_path, 'r', encoding="utf-8") as file:
-            class_schema = json.load(file)
-            log.info("JSON data successfully loaded for class: %s", class_name)
-    except Exception as e:
-        log.error("Error loading JSON file: %s\n%s", e, traceback.format_exc())
-        close_log(log)
-        raise JsonLoaderError(f"Error loading JSON file: {e}") from e
-    finally:
-        close_log(log)
+            self.log.info("""open json in path:
+                           %s""", json_path)
+            return json.load(file)
 
-    return class_schema
-
-def parse_type(log, type_str):
-    """
-    Parses a type string to a SQLAlchemy column type.
-
-    Parameters:
-    type_str (str): The string representing a SQLAlchemy data type.
-
-    Returns:
-    Type: SQLAlchemy column type.
-
-    Raises:
-    Exception: If the type string cannot be parsed.
-    """
-    log.info("Parsing type string: %s", type_str)
-
-    try:
+    @staticmethod
+    def parse_type(self, type_str):
         if '(' in type_str:
             base_type, params = type_str.split('(')
             param = int(params.rstrip(')'))
             return getattr(sqlalchemy, base_type)(param)
         return getattr(sqlalchemy, type_str)
-    except Exception as e:
-        log.error("Error parsing type string: %s\n%s", e, traceback.format_exc())
-        close_log(log)
-        raise
+    
+    @staticmethod
+    def create_repr_string(self, name, columns):
+        self.log.info("""create repr string from
+                      name: %s
+                      columns: %s""", name, columns)
+        repr_parts = [f"{column['name']}='{{self.{column['name']}}}'" if 'String' in column['type'] else f"{column['name']}={{self.{column['name']}}}" for column in columns]
+        standard_parts = ["version_number={self.version_number}", "effective_date='{self.effective_date}'", "expiry_date='{self.expiry_date}'"]
+        return f"<{name}(" + ', '.join(repr_parts + standard_parts) + ")>"
 
-def create_class_schema(json_data):
-    """
-    Creates a class schema from JSON data.
-
-    Parameters:
-    json_data (dict): JSON data containing the schema information.
-
-    Returns:
-    dict: A dictionary of attributes for the class.
-
-    Raises:
-    Exception: If the class schema cannot be created.
-    """
-    log = open_log(json_data['table_name'])
-    log.info("Creating class schema for table: %s", json_data['table_name'])
-
-    try:
+    @staticmethod
+    def create_class_schema(self, json_data):
         attrs = {'__tablename__': json_data['table_name'],
                  '__table_args__': {'extend_existing': True},
-                 '__unique_identifier__': json_data['identifier']}
-        attrs['id'] = Column(Integer, primary_key=True, autoincrement=True)
+                 '__unique_identifier__': json_data['identifier'],
+                 '__column_names__': [column['name'] for column in json_data["columns"]],
+                 'id': Column(Integer, primary_key=True, autoincrement=True)}
+        
+        # Add dynamic __repr__ method
+        repr_string = self.create_repr_string(self, attrs["__tablename__"], json_data['columns'])
+        attrs['__repr__'] = lambda self: repr_string.format(self=self)
 
         for col in json_data['columns']:
-            column_type = parse_type(log, col['type'])
+            column_type = self.parse_type(self, col['type'])
             attrs[col['name']] = Column(column_type)
 
-        # Add standard columns at the end
         attrs['version_number'] = Column(Integer, default=1)
         attrs['effective_date'] = Column(Date, default=datetime.now)
         attrs['expiry_date'] = Column(Date, default=None)
 
-        log.info("Class schema created successfully for table: %s", json_data['table_name'])
-    except Exception as e:
-        log.error("Error creating class schema: %s\n%s", e, traceback.format_exc())
-        raise
-    finally:
-        close_log(log)
-
-    return attrs
-
-def create_class(attrs):
-    """
-    Creates a dynamic class based on the provided attributes.
-
-    Parameters:
-    attrs (dict): Attributes to be included in the class.
-
-    Returns:
-    Class: A dynamically created class.
-
-    Raises:
-    Exception: If the class cannot be created.
-    """
-    log = open_log(attrs['__tablename__'])
-
-    try:
-        log.info("Creating dynamic class for %s table", attrs['__tablename__'])
-        Base = declarative_base()
-
-        # Add dynamic __repr__ method
-        #column_names = [key for key in attrs if not key.startswith('__')]
-        #repr_string = create_repr_string(attrs['__tablename__'], column_names)
-        #attrs['__repr__'] = lambda self: repr_string.format(self=self)
-
-        # Assign functions as class methods
-        #attrs['create_session'] = classmethod(create_session)
-        #attrs['create_table'] = classmethod(create_table)
-        #attrs['add_data'] = classmethod(add_data)
-
-        return type(attrs['__tablename__'], (Base,), attrs)
-    except Exception as e:
-        log.error("Error creating class: %s\n%s", e, traceback.format_exc())
-        raise
-    finally:
-        close_log(log)
-
-def open_class(class_name):
-    """
-    Opens a class by loading its JSON, creating its schema, and then the class itself.
-
-    Parameters:
-    class_name (str): The name of the class to be opened.
-
-    Returns:
-    Class: The dynamically created class.
-
-    Raises:
-    JsonLoaderError: If there is an issue with the JSON loading.
-    """
-    log = open_log(class_name)
-    log.info("Opening %s class logger", class_name)
-
-    try:
-        json_data = load_class_json(class_name)
-        class_attrs = create_class_schema(json_data)
-        table_class = create_class(class_attrs)
-
-        log.info("Class successfully opened: %s", class_name)
-        return table_class
+        return attrs
     
-    except JsonLoaderError as e:
-        log.error("JsonLoaderError encountered: %s", e)
-        raise
-    finally:
-        log.info("Closing %s class logger", class_name)
-        close_log(log)
-
-# Example usage
-regions = open_class("REGIONS")
-regions
+########################################################################################################################
     
+    def update_entries(self, previous_entry, new_entry):
+        """Update the expiry_date of the previous entry."""
+        self.log.info("Update previous entry: %s and new entry: %s", previous_entry, new_entry)
+        previous_entry.expiry_date = datetime.now() - timedelta(days=1)
+        new_entry.version_number = previous_entry.version_number + 1
+
+    def bulk_check_existing_entries(self, session, cls, new_entries):
+        # Extract unique identifiers for all new entries
+        unique_ids = [getattr(instance, cls.__unique_identifier__) for instance in new_entries]
+
+        # Query the database for these identifiers
+        existing_entries = session.query(cls).filter(
+            getattr(cls, cls.__unique_identifier__).in_(unique_ids)
+        ).all()
+
+        # Convert existing entries to a set for easy lookup
+        existing_set = set(getattr(entry, cls.__unique_identifier__) for entry in existing_entries)
+
+        # Return a set of instances that are duplicates
+        return {instance for instance in new_entries if getattr(instance, cls.__unique_identifier__) in existing_set}
+    
+    def bulk_check_previous_versions(self, session, cls, new_entries):
+        # Extract unique identifiers for all new entries
+        unique_ids = [getattr(instance, cls.__unique_identifier__) for instance in new_entries]
+
+        # Query the database for entries with expiry_date None
+        previous_versions = session.query(cls).filter(
+            getattr(cls, cls.__unique_identifier__).in_(unique_ids),
+            cls.expiry_date.is_(None)
+        ).all()
+
+        # Map unique identifiers to previous version instances
+        previous_versions_map = {getattr(entry, cls.__unique_identifier__): entry for entry in previous_versions}
+
+        return previous_versions_map
+
+    def register_before_flush_event(self, session):
+        @event.listens_for(session, "before_flush")
+        def before_flush(session, flush_context, instances):
+            self.log.info("Before flush event triggered")
+
+            new_entries_by_class = defaultdict(list)
+            for instance in session.new:
+                new_entries_by_class[type(instance)].append(instance)
+
+            for cls, new_entries in new_entries_by_class.items():
+                existing_entries = self.bulk_check_existing_entries(session, cls, new_entries)
+                previous_versions = self.bulk_check_previous_versions(session, cls, new_entries)
+
+                for instance in new_entries:
+                    if instance in existing_entries:
+                        self.log.info(f"Duplicate entry found for {instance}. It will not be added to the database.")
+                        session.expunge(instance)
+                    elif getattr(instance, cls.__unique_identifier__) in previous_versions:
+                        previous_entry = previous_versions[getattr(instance, cls.__unique_identifier__)]
+                        self.log.info(f"Previous version exists for {instance}. Updating entries.")
+                        self.update_entries(previous_entry, instance)
+
+    def process_new_entries_for_class(self, session, cls, new_entries):
+        # Bulk check for existing entries and previous versions
+        existing_entries = self.bulk_check_existing_entries(session, cls, new_entries)
+        previous_versions = self.bulk_check_previous_versions(session, cls, new_entries)
+
+        for instance in new_entries:
+            if instance in existing_entries:
+                session.expunge(instance)
+            elif instance in previous_versions:
+                self.update_entries(previous_versions[instance], instance)
+
+    def add_class_methods(self, cls):
+        session = self.session
+
+        @classmethod
+        def init_table(cls):
+            engine = session.get_bind()
+            if not inspect(engine).has_table(cls.__tablename__):
+                cls.metadata.create_all(engine)
+
+        cls.init_table = init_table
+
+        @classmethod
+        def add_data(cls, data):
+            filtered_data = data[cls.__column_names__]
+            for _, row in filtered_data.iterrows():
+                instance = cls(**row.to_dict())
+                session.add(instance)
+            session.commit()
+
+        cls.add_data = add_data
+
+    def open_table(self, class_name):
+        json_data = self.load_class_json(self, class_name)
+        class_attrs = self.create_class_schema(self, json_data)
+        dynamic_class = type(json_data['table_name'], (self.Base,), class_attrs)
+
+        self.add_class_methods(dynamic_class)
+
+        return dynamic_class
